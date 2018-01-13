c098bcebd072d35b3cf97e43e427c91d226ca2ad
Autos4U <dev.autos4u@gmail.com>
2018-01-13 15:30:36 -0200

    Styling cursos-detalhes

--- src/app/cursos/curso-detalhe/curso-detalhe.component.ts
+++ src/app/cursos/curso-detalhe/curso-detalhe.component.ts
@@ -7,7 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CursoDetalheComponent implements OnInit {
 
-  constructor() { }
+  // Atributos
+  titulo: string;
+  horas: number;
+  descricao: string;
+
+  constructor() {
+    this.titulo = 'Angular 5 com PWA';
+    this.horas = 24;
+    this.descricao = 'Curso de Angular para web com PWA';
+  }
 
   ngOnInit() {
   }
