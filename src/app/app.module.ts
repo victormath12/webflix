69854053b7f76218f6e1f0d92b01fb1395bb547b
Autos4U <dev.autos4u@gmail.com>
2018-01-13 14:21:54 -0200

    Creating Cursos Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AppComponent } from './app.component';
-
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import { AppComponent } from './app.component';
+import { CursosModule } from './cursos/cursos.module';
 
 @NgModule({
   declarations: [
@@ -11,6 +12,7 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    CursosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
