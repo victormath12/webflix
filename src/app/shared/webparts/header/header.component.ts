da89ae40d8b690d57c87d0c5d327ae22085a7525
Autos4U <dev.autos4u@gmail.com>
2018-02-08 00:00:59 -0200

    Authentication Login

--- src/app/shared/webparts/header/header.component.ts
+++ src/app/shared/webparts/header/header.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
 import { Observable, Subject } from 'rxjs/Rx';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
 
 @Component({
   selector: 'webflix-header',
@@ -9,22 +11,63 @@ import { Observable, Subject } from 'rxjs/Rx';
 })
 export class HeaderComponent implements OnInit {
 
+  usuarioLogado: Object;
+
   categorias: Observable<any[]>;
 
   private collectionName = 'cursos';
 
   private categoriasCollection: AngularFirestoreCollection<any>;
 
-  constructor(private db: AngularFirestore) {
+  private providerAuth: firebase.auth.GoogleAuthProvider;
+
+  constructor(private db: AngularFirestore, public fbAuth: AngularFireAuth) {
+
     this.categoriasCollection = db.collection<any>(this.collectionName);
+
   }
 
   ngOnInit() {
+
+    /*this.usuarioLogado = {
+      nome: 'Victor',
+      avatar: 'https://avatars2.githubusercontent.com/u/11219999?s=400&u=c75e0601c2c9a64439903662b8f370faa67c6d80&v=4'
+    };*/
+
+    this.providerAuth = new firebase.auth.GoogleAuthProvider();
+    //this.providerAuth.addScope('user');
+
     this.categorias = this.listCategorias();
+
   }
 
   listCategorias(): Observable<any[]> {
     return this.db.collection(this.collectionName).valueChanges();
+  }
+
+  login() {
+
+    this.fbAuth.auth.signInWithPopup(this.providerAuth).then((result) => {
+
+      console.log('Firebase User: ', result.user);
+
+      this.usuarioLogado = {
+        nome: result.user.displayName,
+        email: result.user.email,
+        telefone: result.user.phoneNumber,
+        avatar: result.user.photoURL,
+      };
+
+      console.log(this.usuarioLogado);
+
+    }).catch((error) => {
+      console.log('Erro ao Autenticar: ', error);
+    });
+
+  }
+
+  logout() {
+    this.fbAuth.auth.signOut();
   }
 
 }
